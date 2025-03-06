7e43a311579ea415e52502be746bb25b3a9f0a3e
Sérgio Matheus <sergiomatheuss09@gmail.com>
2025-03-06 15:37:01 -0300

    Finished

---
+++ src/main/java/org/matheus/despesasresidenciais/DespesasResidenciaisApplication.java
@@ -0,0 +1,13 @@
+package org.matheus.despesasresidenciais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DespesasResidenciaisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DespesasResidenciaisApplication.class, args);
+    }
+
+}
